836d01638f2c6431b22250ef7a72eea3a8819ad6
Kevan Haggins <63670071+simplesapien@users.noreply.github.com>
2022-08-16 10:19:35 -0400

    Update parts.js
    Slowly coming together a bit more... I still have to separate the modules into 'displayController' and 'Gameboard' and add a factory function to create the players, but the basic game logic is almost all finished up.

--- parts.js
+++ parts.js
@@ -1,40 +1,66 @@
-const Gameboard = (() => {
+(function () {
 
-    // Initialize gameboard array
-    const board = ['', '', '', '', '', '', '', '', ''];
+    const Gameboard = {
 
-    // Creates a NodeList, and turn into an array to gain the built-in methods attached to arrays
-    const boardSquares = [...document.querySelectorAll('.board-item')];
+        board: ['', '', '', '', '', '', '', '', ''],
 
-    // Take the board array, and render it to the DOM through the boardSquares
-    const render = () => {
-        for (let i = 0; i < board.length; i++) {
-            boardSquares[i].innerHTML = board[i];
+        boardSquares: [...document.querySelectorAll('.board-item')],
+
+        winningCombinations: [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [2, 4, 6], [0, 4, 8]],
+
+        init: function () {
+            this.bindEvents();
+            this.render();
+        },
+
+        bindEvents: function () {
+            this.boardSquares.forEach(element => {
+                element.addEventListener('click', this.updateBoard)
+            });
+        },
+
+        render: function () {
+            for (let i = 0; i < this.board.length; i++) {
+                this.boardSquares[i].innerHTML = this.board[i];
+            }
+        },
+
+        updateBoard: function () {
+
+            // Save current context from previous call (i.e. the boardsquare that called this function)
+            const that = this;
+
+            // Invoke new context attached to the Gameboard object, and find the index of the the element that has been clicked, re-render the board
+            (function () {
+                let index = this.boardSquares.indexOf(that);
+                this.board[index] = 'X';
+                this.render();
+                this.checkWinner();
+            }).call(Gameboard)
+
+        },
+
+        checkWinner: function () {
+
+            // Keep track of the marked board pieces
+            const marked = [];
+            for (let i = 0; i < this.board.length; i++) {
+                if (this.board[i]) marked.push(i);
+            }
+
+            // A function to check whether all items in the second argument are present within the first
+            let checker = (arr, target) => target.every(v => arr.includes(v));
+
+            // Loop through all possible winning combinations, and check if they match the marked items
+            for (let i = 0; i < this.winningCombinations.length; i++) {
+                if (checker(marked, this.winningCombinations[i])) alert('winner');
+            }
+
         }
+
     }
 
-    const updateBoard = () => {
-        console.log(this);
-        // const index = boardSquares.indexOf(this)
-        // this.innerHTML = 'X';
-        // board[index] = 'X';
-    }
-
-    return { render, board, boardSquares, updateBoard }
-
-})();
-
-
-const displayController = (() => {
-
-    const { boardSquares, board, updateBoard } = Gameboard;
-
-    // Add event listener to each board piece
-    boardSquares.forEach(element => {
-        element.addEventListener('click', updateBoard.bind(element))
-    });
-
+    Gameboard.init();
 
 
 })();
-
