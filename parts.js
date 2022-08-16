6e729ed0784e5bbb665a86670a55bdc7db19934a
Kevan Haggins <63670071+simplesapien@users.noreply.github.com>
2022-08-16 15:38:08 -0400

    Update parts.js
    Changed it to a revealing module pattern, not 100% sure on the pattern layout so far.. I tried to keep anything vulnerable out of the global scope, but updateBoard can still be called with whatever but it doesn't seem to be that big of an issue..

--- parts.js
+++ parts.js
@@ -1,66 +1,73 @@
-(function () {
+const personFactory = (name) => {
 
-    const Gameboard = {
+}
 
-        board: ['', '', '', '', '', '', '', '', ''],
+const Gameboard = (() => {
 
-        boardSquares: [...document.querySelectorAll('.board-item')],
+    const board = ['', '', '', '', '', '', '', '', ''];
+    const winningPatterns = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [2, 4, 6], [0, 4, 8]];
 
-        winningCombinations: [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [2, 4, 6], [0, 4, 8]],
+    function updateBoard(index, player) {
 
-        init: function () {
-            this.bindEvents();
-            this.render();
-        },
+        let clickedPiece = document.querySelector(`.board :nth-child(${index + 1})`)
 
-        bindEvents: function () {
-            this.boardSquares.forEach(element => {
-                element.addEventListener('click', this.updateBoard)
-            });
-        },
-
-        render: function () {
-            for (let i = 0; i < this.board.length; i++) {
-                this.boardSquares[i].innerHTML = this.board[i];
-            }
-        },
-
-        updateBoard: function () {
-
-            // Save current context from previous call (i.e. the boardsquare that called this function)
-            const that = this;
-
-            // Invoke new context attached to the Gameboard object, and find the index of the the element that has been clicked, re-render the board
-            (function () {
-                let index = this.boardSquares.indexOf(that);
-                this.board[index] = 'X';
-                this.render();
-                this.checkWinner();
-            }).call(Gameboard)
-
-        },
-
-        checkWinner: function () {
-
-            // Keep track of the marked board pieces
-            const marked = [];
-            for (let i = 0; i < this.board.length; i++) {
-                if (this.board[i]) marked.push(i);
-            }
-
-            // A function to check whether all items in the second argument are present within the first
-            let checker = (arr, target) => target.every(v => arr.includes(v));
-
-            // Loop through all possible winning combinations, and check if they match the marked items
-            for (let i = 0; i < this.winningCombinations.length; i++) {
-                if (checker(marked, this.winningCombinations[i])) alert('winner');
-            }
-
+        if (player == 'player1') {
+            board[index] = 'X';
+            clickedPiece.innerHTML = 'X'
+        } else {
+            board[index] = 'O';
+            clickedPiece.innerHTML = 'O'
         }
+
+        _checkWinner();
 
     }
 
-    Gameboard.init();
+    function _checkWinner() {
 
+        // Keep track of the marked board pieces
+        const playerOne = [];
+        const playerTwo = [];
+
+        // Differentiate between marks placed by players
+        for (let i = 0; i < board.length; i++) {
+            if (board[i] == 'X') playerOne.push(i);
+            if (board[i] == 'O') playerTwo.push(i);
+        }
+
+        let total = playerOne.concat(playerTwo);
+        if (total == board.length) console.log('tie!');
+
+        // A function to check whether all items in the second argument are present within the first
+        const checker = (arr, target) => target.every(v => arr.includes(v));
+
+        for (let i = 0; i < winningPatterns.length; i++) {
+            if (checker(playerOne, winningPatterns[i])) {
+                console.log('Player one winner');
+            } else if (checker(playerTwo, winningPatterns[i])) {
+                console.log('player two wins');
+            }
+        }
+    }
+
+    return { updateBoard }
+
+})();
+
+const displayController = (() => {
+
+    const { updateBoard } = Gameboard;
+
+    const boardSquares = [...document.querySelectorAll('.board-item')];
+
+    boardSquares.forEach(element => {
+        element.addEventListener('click', _updateBoard)
+    });
+
+    function _updateBoard() {
+        let index = boardSquares.indexOf(this);
+        let player = 'player1';
+        updateBoard(index, player);
+    };
 
 })();
