f3580fb4146b8026b73baa09cde05f30baad08d6
Kevan Haggins <63670071+simplesapien@users.noreply.github.com>
2022-08-16 21:53:49 -0400

    Update and rename parts.js to script.js

--- script.js
+++ script.js
@@ -8,17 +8,17 @@ const playerTwo = Player('player2', 'O');
 
 const Gameboard = (() => {
 
-    const board = ['', '', '', '', '', '', '', '', ''];
     const winningPatterns = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [2, 4, 6], [0, 4, 8]];
 
     function updateBoard(index, player) {
 
         // Update DOM
-        let clickedPiece = document.querySelector(`.board :nth-child(${index + 1})`)
+        let clickedPiece = document.querySelector(`.board :nth-child(${index + 1}) > span`)
+        clickedPiece.className = 'fadeIn';
         clickedPiece.innerHTML = player.mark;
 
+
         // Update internal gameboard + the player's array of played pieces
-        board[index] = player.mark;
         player.markers.push(index);
 
         _checkWinner();
@@ -27,8 +27,8 @@ const Gameboard = (() => {
 
     function _checkWinner() {
 
+        let winner;
         let total = playerOne.markers.concat(playerTwo.markers);
-        if (total == board.length) console.log('tie!');
 
         // A function to check whether all items in the second argument are present within the first
         const checker = (arr, target) => target.every(v => arr.includes(v));
@@ -36,10 +36,14 @@ const Gameboard = (() => {
         for (let i = 0; i < winningPatterns.length; i++) {
             if (checker(playerOne.markers, winningPatterns[i])) {
                 console.log('Player one winner');
+                winner = playerOne;
             } else if (checker(playerTwo.markers, winningPatterns[i])) {
                 console.log('player two wins');
+                winner = playerTwo;
             }
         }
+
+        if (total.length == 9 && !winner) console.log('tie!');
     }
 
     return { updateBoard }
@@ -56,10 +60,15 @@ const displayController = (() => {
         element.addEventListener('click', _updateBoard)
     });
 
-    let player = playerTwo;
+    let player;
+    function _changeTurns() {
+        if (player == playerOne) player = playerTwo;
+        else { player = playerOne }
+    }
 
     function _updateBoard() {
         let index = boardSquares.indexOf(this);
+        _changeTurns();
         updateBoard(index, player);
     };
 
